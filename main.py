ddbcab62eb5120cb83cceade609fc7c22812018c
aaloochipsi <aaloochipsi@gmail.com>
2025-05-07 01:14:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hellow World')
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1  
